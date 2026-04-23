11d1782340a3b93c7b5dab7ce450040b38752aa6
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 21:32:16 +0800

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        O(N), but we used division
+        """
+        prod = 1
+        zero_cnt = 0
+        res = [0] * len(nums)
+
+        for num in nums:
+            if num:
+                prod *= num
+            else:
+                zero_cnt += 1
+        
+        if zero_cnt > 1:
+            return res
+        
+        for i, num in enumerate(nums):
+            if zero_cnt == 1:
+                if num == 0:
+                    res[i] = prod
+            else:
+                res[i] = int(prod / num)
+
+        return res
