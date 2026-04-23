8fb254bef2ec91d36136b95695fa91aa86d0b727
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-23 14:25:34 +0800

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        O(N.KlogK)
+        """
+        from collections import defaultdict
+
+        res = defaultdict(list)
+
+        for s in strs:
+            key = tuple(sorted(s))
+            res[key].append(s)
+
+        return list(res.values())
