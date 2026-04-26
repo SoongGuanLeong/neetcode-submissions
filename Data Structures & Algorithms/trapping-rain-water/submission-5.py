b24068b79845ebc33f8c6bf8dd7105f467d31c5d
SoongGuanLeong <108955118+SoongGuanLeong@users.noreply.github.com>
2026-04-26 13:04:05 +0800

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        O(N) - two pointers
+        """
+        n = len(height)
+        if n == 0:
+            return 0
+
+        volume = 0
+
+        l, r = 0, n - 1
+        max_l = height[l]
+        max_r = height[r]
+        while l < r:
+            if max_l <= max_r:
+                l += 1
+                max_l = max(max_l, height[l])       # this remove the need to check for negative boundary
+                volume += max_l - height[l]
+            else:
+                r -= 1
+                max_r = max(max_r, height[r])
+                volume += max_r - height[r]
+        return volume
